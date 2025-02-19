bd61f5d45ddedc85d20c707816c199447d7cf445
Sarthak Kumbhar <sarthakkumbhar43@gamil.com>
2025-02-19 15:07:34 +0530

    "updated"

---
+++ src/landingPage/components/news.jsx
@@ -0,0 +1,14 @@
+import './news.css'
+export const News = () => {
+  return (
+    <>
+      <div className="scroll-container" id="scroll-container">
+        <div className="snap-item " id="n1"></div>
+        <div className="snap-item " id="n2"></div>
+        <div className="snap-item " id="n3"></div>
+        <div className="snap-item " id="n4"></div>
+        <div className="snap-item " id="n5"></div>
+      </div>
+    </>
+  )
+}
--- src/landingPage/video.jsx
+++ src/landingPage/video.jsx
@@ -1,11 +1,17 @@
 import { Video } from "./components/video";
 import { Search } from "./components/search";
+import { News } from "./components/news";
 
 const Banner = () => {
 
   return (
     <div className="container h-150 mx-auto p-8 flex justify-start gap-8 main">
-      <Search />
+
+      <div className="w-full h-full ">
+        <Search />
+        <News />
+      </div>
+      
 
       <Video />
     </div>
