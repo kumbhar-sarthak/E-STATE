c790417b6cbf52dc4fed120a8d332b343cb14d67
Sarthak Kumbhar <sarthakkumbhar43@gamil.com>
2025-02-20 21:59:18 +0530

    "arrows updated"

--- src/landingPage/components/news.jsx
+++ src/landingPage/components/news.jsx
@@ -1,4 +1,4 @@
-import './news.css'
+import "./news.css";
 export const News = () => {
   return (
     <>
@@ -8,7 +8,43 @@ export const News = () => {
         <div className="snap-item " id="n3"></div>
         <div className="snap-item " id="n4"></div>
         <div className="snap-item " id="n5"></div>
+        <div className="absolute bottom-0 right-0 mr-4 mb-4">
+          <div className="w-[25px] h-[25px] rounded-2xl border border-white cursor-pointer flex justify-center items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              class="size-4"
+              color="white"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m4.5 15.75 7.5-7.5 7.5 7.5"
+              />
+            </svg>
+          </div>
+          <div className="w-[25px] h-[25px] rounded-2xl border border-white mt-2 cursor-pointer flex justify-center items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              class="size-4"
+              color="white"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
